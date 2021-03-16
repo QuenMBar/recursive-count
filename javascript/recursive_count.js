7b0bc392f865ba914f495e7f77d50aa9669ff60b
QuenMBar <Qbeast360@gmail.com>
2021-03-16 14:10:15 -0700

    Compleate

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,9 +1,13 @@
 function recursiveCount(num = 0) {
-  // type your code here
+    if (num < 10) {
+        console.log(num);
+        ++num;
+        recursiveCount(num);
+    }
 }
 
 if (require.main === module) {
-  recursiveCount();
+    recursiveCount();
 }
 
 module.exports = recursiveCount;
